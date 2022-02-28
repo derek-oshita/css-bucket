c79de9df5f0a24141ba2c98a4630da20728ae6a6
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2022-02-27 20:46:42 -0800

    Desktop version done.

--- landingPages/birthday_card/app.js
+++ landingPages/birthday_card/app.js
@@ -1,5 +1,5 @@
 const imageSection = document.getElementById("image-section");
-const img = document.querySelector("img");
+const img = document.getElementById("image");
 const randomIdx = Math.floor(Math.random() * 11);
 const imageArray = [
   "static_photos/chelsea2011.jpeg",
@@ -8,12 +8,12 @@ const imageArray = [
   "static_photos/chelsea2014.jpeg",
   "static_photos/chelsea2015.jpeg",
   "static_photos/chelsea2016.jpeg",
-  "static_photos/chelsea2017.jpeg",
   "static_photos/chelsea2018.jpeg",
   "static_photos/chelsea2018(2).jpeg",
   "static_photos/chelsea2019.jpeg",
   "static_photos/chelsea2020.jpeg",
   "static_photos/chelsea2021.jpeg",
+  "static_photos/chelsea2022.jpeg",
 ];
 
 img.src = `${imageArray[randomIdx]}`;
