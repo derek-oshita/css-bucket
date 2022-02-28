ff4d20b72dfe4d3bcdb2cd123052bfe38e74f1d7
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2022-02-27 16:00:23 -0800

    Image randomly populates.

--- landingPages/birthday_card/app.js
+++ landingPages/birthday_card/app.js
@@ -1,1 +1,19 @@
-console.log("balthasar");
+const imageSection = document.getElementById("image-section");
+const img = document.querySelector("img");
+const randomIdx = Math.floor(Math.random() * 11);
+const imageArray = [
+  "static_photos/chelsea2011.jpeg",
+  "static_photos/chelsea2012.jpeg",
+  "static_photos/chelsea2013.jpeg",
+  "static_photos/chelsea2014.jpeg",
+  "static_photos/chelsea2015.jpeg",
+  "static_photos/chelsea2016.jpeg",
+  "static_photos/chelsea2017.jpeg",
+  "static_photos/chelsea2018.jpeg",
+  "static_photos/chelsea2018(2).jpeg",
+  "static_photos/chelsea2019.jpeg",
+  "static_photos/chelsea2020.jpeg",
+  "static_photos/chelsea2021.jpeg",
+];
+
+img.src = `${imageArray[randomIdx]}`;
