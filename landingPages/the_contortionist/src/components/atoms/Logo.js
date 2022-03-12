2a15ab90befd81ec68441cddf9b3d0863634276c
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2022-03-11 16:05:54 -0800

    Removed the reset animation on the logo.

--- landingPages/the_contortionist/src/components/atoms/Logo.js
+++ landingPages/the_contortionist/src/components/atoms/Logo.js
@@ -1,19 +1,13 @@
-import { useState } from "react";
 import { useSpring, animated, config } from "react-spring";
-import PropTypes from "prop-types";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 export default function Logo() {
-  const [flip, set] = useState(false);
-  const props = useSpring({
+  const animatedProps = useSpring({
     to: { opacity: 1 },
     from: { opacity: 0 },
-    reset: true,
-    reverse: flip,
     delay: 200,
     config: config.molasses,
-    onRest: () => set(!flip),
   });
   return (
     <Box sx={{ p: 2 }}>
@@ -26,15 +20,10 @@ export default function Logo() {
           boxShadow: "none",
         }}
       >
-        <animated.h4 style={props}>THE CONTORTIONIST</animated.h4>;
-        {/* THE CONTORTIONIST */}
+        <animated.h4 style={animatedProps}>THE CONTORTIONIST</animated.h4>;
       </Typography>
     </Box>
   );
-}
-
-function Text() {
-  return;
 }
 
 Logo.propTypes = {};
